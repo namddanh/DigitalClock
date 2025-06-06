e18a690bd189600634104dcf62935decc73d5efd
Nam <namddanh@gmail.com>
2025-06-05 20:31:20 -0400

    Styling changes as well as clock format 12 hours added

--- src/js/clock.js
+++ src/js/clock.js
@@ -1,13 +1,14 @@
 let clock = document.getElementById('time');
+let is12HourFormat = true;
 
 function updateClock() {
   const now = new Date(); // Thu Jun 05 2025 13:01:56 GMT-0400 (Eastern Daylight Time)
   const timeString = now.toLocaleTimeString([], {
-    hour: '2-digit',
+    hour: 'numeric',
     minute: '2-digit',
-    hour12: true,
+    hour12: is12HourFormat,
   });
-  console.log(timeString);
+
   clock.textContent = timeString;
 }
 
